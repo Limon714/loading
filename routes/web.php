1c5e6554367e31ff2580356ce3bfa541ec28c522
Limon Islam <limon4u714@gmail.com>
2024-01-28 22:53:42 +0600

    JWT Token add for User Authentication

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\PolicyController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\TokenAuthenticate;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +26,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Brand List
+Route::get('/BrandList', [BrandController::class, 'BrandList']);
+// Category List
+Route::get('/CategoryList', [CategoryController::class, 'CategoryList']);
+// Product List
+Route::get('/ListProductByCategory/{id}', [ProductController::class, 'ListProductByCategory']);
+Route::get('/ListProductByBrand/{id}', [ProductController::class, 'ListProductByBrand']);
+Route::get('/ListProductByRemark/{remark}', [ProductController::class, 'ListProductByRemark']);
+// Slider
+Route::get('/ListProductSlider', [ProductController::class, 'ListProductSlider']);
+// Product Details
+Route::get('/ProductDetailsById/{id}', [ProductController::class, 'ProductDetailsById']);
+Route::get('/ListReviewByProduct/{product_id}', [ProductController::class, 'ListReviewByProduct']);
+//policy
+Route::get("/PolicyByType/{type}",[PolicyController::class,'PolicyByType']);
+
+
+// User Auth
+Route::get('/UserLogin/{UserEmail}', [UserController::class, 'UserLogin']);
+Route::get('/VerifyLogin/{UserEmail}/{OTP}', [UserController::class, 'VerifyLogin']);
+Route::get('/logout',[UserController::class,'UserLogout']);
